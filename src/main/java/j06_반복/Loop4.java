4d0993cfa6281565281b3aa3bf003a8316aa3889
kyeongwonMoon <mkw1999@naver.com>
2022-11-17 12:55:29 +0900

    챕터 6 반복

---
+++ src/main/java/j06_반복/Loop4.java
@@ -0,0 +1,10 @@
+package j06_반복;
+
+public class Loop4 {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/main/java/j06_반복/Loop5.java
@@ -0,0 +1,23 @@
+package j06_반복;
+
+public class Loop5 {
+
+    public static void main(String[] args) {
+
+        boolean loopFlag = true;
+
+        while (loopFlag) {
+            String select = null;
+
+            System.out.println("1. 덧셈");
+            System.out.println("2. 뺄셈");
+            System.out.println("3. 곱셈");
+            System.out.println("4. 나눗셈");
+            System.out.println("5. 나머지");
+            System.out.println("q. 프로그램 종료");
+            System.out.print("선택: ");
+
+        }
+
+    }
+}
