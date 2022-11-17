4e521f55c3396974f7efbcbfa4cff3e03e12ded0
kyeongwonMoon <mkw1999@naver.com>
2022-11-17 12:56:34 +0900

    챕터 9 접근지정자

---
+++ src/main/java/j09_접근지정자/test/Access3.java
@@ -0,0 +1,11 @@
+package j09_접근지정자.test;
+
+public class Access3 {
+
+    private String address;
+
+    private void printAddress(){
+        System.out.println("address : " + address);
+    }
+
+}
