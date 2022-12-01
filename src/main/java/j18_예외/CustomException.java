4fe2a955c710bf2a15aaadc742f0c586a1a623bf
문경원 <mkw1999@naver.com>
2022-12-01 12:52:25 +0900

    챕터 18 예외

---
+++ src/main/java/j18_예외/CustomException.java
@@ -0,0 +1,41 @@
+package j18_예외;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class CustomException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	Map<String, String> errorMap;
+	
+	public CustomException() {
+		super();
+	}
+	
+	public CustomException(String message) {
+		super(message);
+	}
+	
+	public CustomException(Map<String,String> errorMap) {
+		super();
+		this.errorMap = errorMap;
+	}
+	
+	public CustomException(String message, Map<String, String> errorMap) {
+		super(message);
+		this.errorMap = errorMap;
+	}
+	
+	public void printErrorMap() {
+		Objects.requireNonNull(errorMap);
+		for(Map.Entry<String, String> entry : errorMap.entrySet()) {
+			System.out.println(entry.getKey() + ">>>" + entry.getValue());
+		}
+		System.out.println();
+	}
+
+}
